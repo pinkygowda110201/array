2ba1a9fa71d6fb0e1b29f030f22d2b2ab4fa477a
pinkygowda110201 <71083770+pinkygowda110201@users.noreply.github.com>
2020-09-20 20:15:15 +0530

    Update array.c

--- array.c
+++ array.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 main()
 {
-	int mat[3][3];
+	int mat[3][3], *ptr[3][3];
 	int i,j;
-	printf("Input elements int the matrix:\n");
+	printf("Input elements in the matrix:\n");
 	for(i=0;i<3;i++)
 	{
 		for(j=0;j<3;j++)
 		{
 			printf("Element-[%d],[%d] : ",i,j);
-			scanf("%d",(*(mat+i)+j));
+			scanf("%d",&mat[i][j]);
+			ptr[i][j]=&mat[i][j];
 		}
 	}
 	printf("\nThe matrix is :\n");
@@ -17,7 +18,7 @@ main()
 	{
 		for(j=0;j<3;j++)
 		{
-			printf("%d\t",*(*(mat+i)+j));
+			printf("%d\t",*ptr[i][j]);
 		}
 		printf("\n");
 	}
